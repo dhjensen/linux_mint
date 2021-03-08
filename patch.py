8639402aee2d82b76e9601756d58c915126b1686
Daniel Jensen <dhjen@outlook.com>
2021-03-08 11:43:51 +0100

    Change indendation

--- patch.py
+++ patch.py
@@ -1,102 +1,125 @@
+'''
+This module create overrides yaml files
+'''
+
 import yaml
 
-# Content to add in overrides
-def add(content, name, items=[]):
+def add(content, name, items=None):
+    '''
+    Content to add in overrides
+    '''
 
-  return_content = {name: content}
+    return_content = {name: content}
 
-  for item in items:
-    return_content[name].append(item)
+    if items:
+        for item in items:
+            return_content[name].append(item)
 
-  return return_content
+    return return_content
 
 def remove(target, remove_items):
+    '''
+    Remove unwanted items from the list
+    '''
+    for item in remove_items:
+        target.remove(item)
 
-  for item in remove_items:
-    target.remove(item)
-
-  return target
+    return target
 
 def create_filename(name):
-  filename = 'mint20_override_' + name + '.yaml'
-  return filename
+    '''
+    Creates output filenames
+    '''
+    filename = 'mint20_override_' + name + '.yaml'
+    return filename
 
 def write_yaml_file(filename, output):
-  with open(filename, 'w') as file:
-    yaml.dump(output, file, explicit_start=True, explicit_end=True, sort_keys=False)
+    '''
+    Write yaml content to disk
+    '''
+    with open(filename, 'w') as file:
+        yaml.dump(output, file, explicit_start=True, explicit_end=True, sort_keys=False)
 
 def main():
-  # names of sections to work on.
-  name_deb = 'deb'
-  name_downloads = 'downloads'
-  name_files = 'files'
-  name_flatpak = 'flatpak'
-  name_pip = 'pip'
-  name_startup = 'startup'
+    '''
+    I'm main :)
+    '''
+    # names of sections to work on.
+    name_deb = 'deb'
+    name_downloads = 'downloads'
+    name_files = 'files'
+    name_flatpak = 'flatpak'
+    name_pip = 'pip'
+    name_startup = 'startup'
 
-  # Content to add
-  add_deb = [
-    'https://github.com/BoostIO/BoostNote.next/releases/download/v0.13.2/boost-note-linux.deb',
-    'https://github.com/bitwarden/desktop/releases/download/v1.24.6/Bitwarden-1.24.6-amd64.deb',
-    'https://download.teamviewer.com/download/linux/teamviewer_amd64.deb'
-  ]
-  add_downloads = [
-    {'url': 'https://github.com/bitwarden/cli/releases/download/v1.13.3/bw-linux-1.13.3.zip', 'destination': 'bw', 'skip_tree': False}
-  ]
-  add_files = [
-    {
-      'url': 'https://github.com/ytmdesktop/ytmdesktop/releases/download/v1.12.1/YouTube.Music.Desktop.App-1.12.1.AppImage',
-      'destination': 'youtubemusic',
-      'desktop_file': './files/apps/youtubemusic/youtubemusic.desktop'
-    },
-    {
-      'url': 'https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-1.1.5-linux.AppImage',
-      'destination': 'yubikeymanager',
-      'desktop_file': './files/apps/yubikeymanager/yubikeymanager.desktop'
-    }
-  ]
-  add_flatpak = [
-    {'name': 'https://flathub.org/repo/appstream/org.telegram.desktop.flatpakref'},
-    {'name': 'https://flathub.org/repo/appstream/org.signal.Signal.flatpakref'},
-    {'name': 'https://flathub.org/repo/appstream/us.zoom.Zoom.flatpakref'}
-  ]
-  add_pip = [
-    'pylint'
-  ]
+    # Content to add
+    # pylint: disable=line-too-long
+    add_deb = [
+        'https://github.com/BoostIO/BoostNote.next/releases/download/v0.13.2/boost-note-linux.deb',
+        'https://github.com/bitwarden/desktop/releases/download/v1.24.6/Bitwarden-1.24.6-amd64.deb',
+        'https://download.teamviewer.com/download/linux/teamviewer_amd64.deb'
+    ]
+    add_downloads = [
+        {
+            'url': 'https://github.com/bitwarden/cli/releases/download/v1.13.3/bw-linux-1.13.3.zip',
+            'destination': 'bw',
+            'skip_tree': False
+        }
+    ]
+    add_files = [
+        {
+        'url': 'https://github.com/ytmdesktop/ytmdesktop/releases/download/v1.12.1/YouTube.Music.Desktop.App-1.12.1.AppImage',
+        'destination': 'youtubemusic',
+        'desktop_file': './files/apps/youtubemusic/youtubemusic.desktop'
+        },
+        {
+        'url': 'https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-1.1.5-linux.AppImage',
+        'destination': 'yubikeymanager',
+        'desktop_file': './files/apps/yubikeymanager/yubikeymanager.desktop'
+        }
+    ]
+    add_flatpak = [
+        {'name': 'https://flathub.org/repo/appstream/org.telegram.desktop.flatpakref'},
+        {'name': 'https://flathub.org/repo/appstream/org.signal.Signal.flatpakref'},
+        {'name': 'https://flathub.org/repo/appstream/us.zoom.Zoom.flatpakref'}
+    ]
+    add_pip = [
+        'pylint'
+    ]
+    # pylint: enable=line-too-long
 
-  # Content to remove in overrides
-  remove_startup = [
-    {'filename': 'dropbox.desktop', 'source': './files/apps/dropbox/dropbox.desktop'},
-    {'filename': 'synapse.desktop', 'source': './files/apps/synapse/synapse.desktop'},
-    {'filename': 'DockX.desktop', 'source': './files/apps/dockbarx/DockX.desktop'}
-  ]
+    # Content to remove in overrides
+    remove_startup = [
+        {'filename': 'dropbox.desktop', 'source': './files/apps/dropbox/dropbox.desktop'},
+        {'filename': 'synapse.desktop', 'source': './files/apps/synapse/synapse.desktop'},
+        {'filename': 'DockX.desktop', 'source': './files/apps/dockbarx/DockX.desktop'}
+    ]
 
-  # Assign content we care about to variables
-  with open('mint20.yaml') as file:
-    content = yaml.load(file, Loader=yaml.FullLoader)
-    deb = content['deb']
-    downloads = content['downloads']
-    files = content['files']
-    flatpak = content['flatpak']
-    pip = content['pip']
-    startup = content['startup']
+    # Assign content we care about to variables
+    with open('mint20.yaml') as file:
+        content = yaml.load(file, Loader=yaml.FullLoader)
 
-  # Remove startup content before creating output.
-  remove(startup, remove_startup)
+    # Remove startup content before creating output.
+    remove(content['startup'], remove_startup)
 
-  output_deb = add(deb, name_deb, add_deb)
-  output_downloads = add(downloads, name_downloads, add_downloads)
-  output_files = add(files, name_files, add_files)
-  output_flatpak = add(flatpak, name_flatpak, add_flatpak)
-  output_pip = add(pip, name_pip, add_pip)
-  output_startup = add(startup, name_startup)
-
-  write_yaml_file(create_filename(name_deb), output_deb)
-  write_yaml_file(create_filename(name_downloads), output_downloads)
-  write_yaml_file(create_filename(name_files), output_files)
-  write_yaml_file(create_filename(name_flatpak), output_flatpak)
-  write_yaml_file(create_filename(name_pip), output_pip)
-  write_yaml_file(create_filename(name_startup), output_startup)
+    write_yaml_file(create_filename(name_deb),
+        add(content['deb'], name_deb, add_deb)
+    )
+    write_yaml_file(create_filename(name_downloads),
+        add(content['downloads'], name_downloads, add_downloads)
+    )
+    write_yaml_file(create_filename(name_files),
+        add(content['files'], name_files, add_files)
+    )
+    write_yaml_file(create_filename(name_flatpak),
+        add(content['flatpak'], name_flatpak, add_flatpak)
+    )
+    write_yaml_file(create_filename(name_pip),
+        add(content['pip'], name_pip, add_pip)
+    )
+    write_yaml_file(create_filename(name_startup),
+        add(content['startup'], name_startup)
+    )
 
 if __name__ == "__main__":
     # execute only if run as a script
